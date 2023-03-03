9e1c956f636b1d6c9a9edb35707d2ed4e2628549
Echo <576520923@qq.com>
2023-03-04 00:21:45 +0800

    创建bean并且测试

---
+++ spring6-first/src/main/java/top/echoyan0924/spring6/TestUser.java
@@ -0,0 +1,21 @@
+package top.echoyan0924.spring6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestUser {
+
+    @Test
+    public void testUserObject(){
+        //加载spring配置文件,对象创建
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("bean.xml");
+
+        //获取创建的对象
+        User user = (User)context.getBean("user");
+        System.out.println(user);
+        //使用对象调用方法进行测试
+        user.add();
+    }
+}
