74b61485f471f0e2fb37ba25ffb9f4c0a8d34d1b
Echo <576520923@qq.com>
2023-03-06 19:19:59 +0800

    新增日志log4j2

--- spring6-first/src/main/java/top/echoyan0924/spring6/TestUser.java
+++ spring6-first/src/main/java/top/echoyan0924/spring6/TestUser.java
@@ -1,10 +1,15 @@
 package top.echoyan0924.spring6;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestUser {
+
+    private Logger logger = LoggerFactory.getLogger("TestUser.class");
+
 
     @Test
     public void testUserObject(){
@@ -14,8 +19,13 @@ public class TestUser {
 
         //获取创建的对象
         User user = (User)context.getBean("user");
-        System.out.println(user);
+        System.out.println("1:"+user);
         //使用对象调用方法进行测试
+        System.out.println("2:");
         user.add();
+
+        //手动写日志
+        logger.info("#### 执行调用成功");
+
     }
 }
